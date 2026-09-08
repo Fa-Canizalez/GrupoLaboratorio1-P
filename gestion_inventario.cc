1519156ce2783e80fbc597098af83217cb838f5c
jos3alvarez <00060326@uca.edu.sv>
2026-09-08 14:43:18 -0600

    consultar stock

--- gestion_inventario.cc
+++ gestion_inventario.cc
@@ -1,7 +1,38 @@
 #include <iostream>
 
-int main ()
+struct Stock
 {
+    int cantidad_stock;
+};
+
+
+void ConsultarStock(int carga);
+
+
+int main()
+{
+    Stock stock;
+    stock.cantidad_stock = 50;
+
+    std::cout << "Cantidad inicial:" << std::endl;
+    ConsultarStock(stock.cantidad_stock);
+
+   
+
+    
+     ConsultarStock(stock.cantidad_stock);
+
+   
 
     return 0;
 }
+
+
+void ConsultarStock(int stock)
+{
+    std::cout << "Cantidad actual: " << stock  << std::endl;
+
+}
+
+
+
